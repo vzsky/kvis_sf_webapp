d0c6da32ee45a01b655666376fb2f99801796494
Touch Sungkawichai <mb99n@Touchs-MacBook-Pro.local>
2020-01-11 21:51:02 +0700

    link db + update

---
+++ src/old_views/home.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { link } from "svelte-spa-router";
+</script>
+
+<style>
+
+</style>
+
+<section class="w-screen h-screen flex flex-col justify-center">
+  <h1 class="flex-inital text-5xl text-center">KVIS Science Fair Admin Menu</h1>
+  <h5 class="flex-inital text-xl text-center">
+    Go to
+    <a href="/admin" use:link class="text-blue-700 inline">admin page</a>
+  </h5>
+</section>
---
+++ src/views/base.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { link } from "svelte-spa-router";
+</script>
+
+<meta name='viewport' content='width=divice-width, initial-scale'>
+
+<!-- <header class="flex item-center px-3 py-3 justify-between bg-gray-900">
+  <div>
+    <a href="http://app.kvis.ac.th/issf2020">
+      <img class="h-8" src="/images/kvis_white.png" alt="kvis">
+    </a> 
+  </div> 
+</header> -->
+
+<slot />
